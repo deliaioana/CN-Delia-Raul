f023c3c0df342c2dc91635adb2fa0f8e5e368243
Raul-Madalin <raul05madalin@yahoo.com>
2023-03-13 16:06:45 +0200

    added structure

---
+++ Tema2/project/main.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def is_zero(number):
+    pass
+
+
+def calculate_D():
+    pass
+
+
+def calculate_L():
+    pass
+
+
+def calculate_Z():
+    pass
+
+
+def calculate_Y():
+    pass
+
+
+def calculate_X():
+    pass
+
+
+def run():
+    pass
