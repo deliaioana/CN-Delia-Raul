dc35d8607fd37031fa0c3aa3c4f0db21feeef28d
Raul-Madalin <raul05madalin@yahoo.com>
2023-05-01 18:29:21 +0300

    Added structure

---
+++ Tema8/project/main.py
@@ -0,0 +1,26 @@
+# HK 8
+PRECISION = 10 ** (-6)
+
+
+def get_input():
+    pass
+
+
+def secant_method():
+    pass
+
+
+def first_derivative_v1():
+    pass
+
+
+def first_derivative_v2():
+    pass
+
+
+def second_derivative():
+    pass
+
+
+def run():
+    get_input()
