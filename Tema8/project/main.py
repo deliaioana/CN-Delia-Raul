fbf9483e7f46d54b3a181d87958fe31785e98fca
Raul-Madalin <raul05madalin@yahoo.com>
2023-05-01 19:24:52 +0300

    Added secant_method

--- Tema8/project/main.py
+++ Tema8/project/main.py
@@ -1,7 +1,9 @@
 # HK 8
-import math
+import numpy as np
 
-PRECISION = 10 ** (-6)
+PRECISION = 10 ** (-5)
+K_MAX = 1000
+
 function_selector = 0
 H = 0
 
@@ -20,7 +22,7 @@ def function_v1(x: float):
 
 
 def function_v2(x: float):
-    return (x ** 2) * math.sin(x)
+    return (x ** 2) * np.sin(x)
 
 
 def function_v3(x: float):
@@ -29,12 +31,57 @@ def function_v3(x: float):
 
 def get_input():
     global function_selector, H
-    function_selector = int(input("Select example: 1 / 2 / 3 ..."))
-    power = int(input("Select power of h: "))
+    function_selector = int(input("Select example: 1 / 2 / 3: "))
+    power = int(input("Select power of h (5 or 6): "))
     H = 10 ** (-power)
 
 
-def secant_method():
+def secant_method_v1():
+    if function_selector == 1:
+        x = 3.41
+        x0 = 3.40
+    elif function_selector == 2:
+        x = -0.44
+        x0 = -0.43
+    elif function_selector == 3:
+        x = 1.99
+        x0 = 1.98
+    else:
+        x = 0
+        x0 = 0
+    k = 0
+
+    delta_x = ((x - x0) * first_derivative_v1(x)) / (first_derivative_v1(x) - first_derivative_v1(x0))
+    if -PRECISION < delta_x < PRECISION:
+        if np.abs(first_derivative_v1(x)) <= PRECISION / 100:
+            delta_x = 0
+        else:
+            delta_x = PRECISION
+
+    x0 = x
+    x = x - delta_x
+    k += 1
+
+    while PRECISION <= np.abs(delta_x) <= (10 ** 8) and k <= K_MAX:
+        delta_x = ((x - x0) * first_derivative_v1(x)) / (first_derivative_v1(x) - first_derivative_v1(x0))
+        if -PRECISION < delta_x < PRECISION:
+            if np.abs(first_derivative_v1(x)) <= PRECISION / 100:
+                delta_x = 0
+            else:
+                delta_x = PRECISION
+
+        x0 = x
+        x = x - delta_x
+        k += 1
+
+    if np.abs(delta_x) < PRECISION:
+        print(f"Solution found: {x} in {k} iterations (first derivative v1)")
+        print(f"Second derivative check: {second_derivative(x) > 0}\n")
+    else:
+        print("Divergenta")
+
+
+def secant_method_v2():
     if function_selector == 1:
         x = 3.41
         x0 = 3.40
@@ -44,7 +91,39 @@ def secant_method():
     elif function_selector == 3:
         x = 0.99
         x0 = 0.98
+    else:
+        x = 0
+        x0 = 0
     k = 0
+
+    delta_x = ((x - x0) * first_derivative_v2(x)) / (first_derivative_v2(x) - first_derivative_v2(x0))
+    if -PRECISION < delta_x < PRECISION:
+        if np.abs(first_derivative_v2(x)) <= PRECISION / 100:
+            delta_x = 0
+        else:
+            delta_x = PRECISION
+
+    x0 = x
+    x = x - delta_x
+    k += 1
+
+    while PRECISION <= np.abs(delta_x) <= (10 ** 8) and k <= K_MAX:
+        delta_x = ((x - x0) * first_derivative_v2(x)) / (first_derivative_v2(x) - first_derivative_v2(x0))
+        if -PRECISION < delta_x < PRECISION:
+            if np.abs(first_derivative_v2(x)) <= PRECISION / 100:
+                delta_x = 0
+            else:
+                delta_x = PRECISION
+
+        x0 = x
+        x = x - delta_x
+        k += 1
+
+    if np.abs(delta_x) < PRECISION:
+        print(f"Solution found: {x} in {k} iterations (first derivative v2)")
+        print(f"Second derivative check: {second_derivative(x) > 0}\n")
+    else:
+        print("Divergenta")
 
 
 def first_derivative_v1(x: float):
@@ -70,3 +149,8 @@ def second_derivative(x: float):
 
 def run():
     get_input()
+    secant_method_v1()
+    secant_method_v2()
+
+
+run()
